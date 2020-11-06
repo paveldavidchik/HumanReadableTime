6b8ce044238cb04eb1ef245ae7d24bbe92b23b6c
paveldavidchik <pdavidchik@gmail.com>
2020-11-07 01:09:23 +0300

    task

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 def make_readable(seconds):
-    return f'{seconds // 3600 :02}:{(seconds % 3600) // 60 :02}:{(seconds % 3600) % 60 :02}'
+    return f'{seconds // 3600 :02}:{(seconds % 3600) // 60 :02}:{seconds % 60 :02}'
 
 
-print(make_readable(60))
+print(make_readable(359999))
